d93289b23aa5ff7c5c926a8f73df075c497a90f6
Sergi_Bogatell_laptop <bogatell.sergi@gmail.com>
2024-11-19 23:37:13 +0100

    instr mem and PC

--- 1_Fetch.v
+++ 1_Fetch.v
@@ -1,5 +1,18 @@
 
-module Fetch(A,B,C);
-input wire A,B;
-output wire C;
+module Fetch(jumpPC,clk);
+
+input wire [4:0]jumpPC;
+input wire clk;
+
+reg [31:0] insMem [31:0];
+reg [4:0] PC;
+
+wire jump; //jump=1 means override the PC
+wire [4:0] newPC;
+assign newPC = jump? (jump):(PC+1);
+
+always @(posedge clk) begin
+    PC=newPC;
+end
+    
 endmodule
