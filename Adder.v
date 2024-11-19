068176112b079edb1bb9bf169b9944a5a9cc25fd
Sergi_Bogatell_laptop <bogatell.sergi@gmail.com>
2024-11-19 22:15:21 +0100

    sim works

--- Adder.v
+++ Adder.v
@@ -1,15 +1,9 @@
 module Adder(
     input wire [3:0] A,
     input wire [3:0] B,
-    output wire [3:0] C,
-    output wire cout   
+    output wire [4:0] C
     );
     
-    wire [4:0] result;
-    assign result = A+B;
-    assign cout = result [4];
-    assign C = result [3:0];
-    
-    
-    
+    assign C = A+B;
+     
 endmodule
--- Adder_tb.v
+++ Adder_tb.v
@@ -2,12 +2,16 @@ module Adder_tb( );
     
     reg clk;
     reg [4:0] result ;
+    wire [4:0] resultw;
     reg [3:0] A ;
     reg [3:0] B ;
     
     always #5 clk = ~clk;
     
-    Adder Adder_1(A,B,result);
+    Adder Adder_1(A,B,resultw);
+    
+    always @(posedge clk)
+        result = resultw;  
     
     initial begin
         #5 clk = 0;
