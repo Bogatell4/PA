c4a6c7661a3cb6de992f00963cff00e63306c3e4
Sergi_Bogatell_laptop <bogatell.sergi@gmail.com>
2024-11-21 14:40:01 +0100

    instr mem access

--- 1_Fetch.v
+++ 1_Fetch.v
@@ -1,18 +1,26 @@
-
-module Fetch(jumpPC,clk);
+module Fetch(jumpPC,clk,insReg);
 
 input wire [4:0]jumpPC;
 input wire clk;
+output reg [31:0] insReg; //final reg of the fetch stage
 
+wire [31:0]insRegwire;
 reg [31:0] insMem [31:0];
 reg [4:0] PC;
 
+
+
+
+//PC count and register
 wire jump; //jump=1 means override the PC
 wire [4:0] newPC;
 assign newPC = jump? (jump):(PC+1);
-
 always @(posedge clk) begin
-    PC=newPC;
-end
-    
+    PC<=newPC;
+    insReg<=insRegwire;
+end 
+
+assign insRegwire = insMem [PC]; //instruction mem Mux controled by PC
+
 endmodule
+
